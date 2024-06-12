efca910da9e633a41f2637cbd0234e668bbf04fa
Muskan <muskangupta9566@gmail.com>
2024-06-12 15:22:02 +0530

    code commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CitySelector from "./components/Country";
+
+function App() {
+  return (
+    <div>
+      <CitySelector />
+    </div>
+  );
+}
+
+export default App;
